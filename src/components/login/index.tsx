ddd6057fc794e2be956459a96db008399bd2d7f4
Roman Yacenko <ryacenko3@gmail.com>
2022-08-03 20:46:51 +0300

    added controlin inputs

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,8 +1,44 @@
 import React, { useState } from 'react';
 import './style/index.scss';
 
+type UserLoginInfo = {
+  email: string,
+  password: string,
+  rememberMe: boolean,
+}
+
 export const Login: React.FC = () => {
-  const [ rememberMe, setRememberMe ] = useState<boolean>(false);
+  const [ userInfo, setUserInfo ] = useState<UserLoginInfo>({
+    email: '',
+    password: '',
+    rememberMe: false,
+  })
+
+
+  const validation = () => {
+    const { email, password, rememberMe } = userInfo;
+
+    if (email.length < 2 || email.length > 50) {
+      // i'll add modal block with error
+      alert('Your email length is not valid');
+      return
+    }
+
+    if (password.length < 8 || password.length > 20) {
+      alert('Your password length is not valid');
+      return
+    }
+
+    if (password.match(/[A-Z]/g)) {
+      alert('Your password must includes one symbol in uppercase')
+      return
+    }
+
+    if (password.match(/&?!-_$#*()/g)) {
+      alert('your password must includes this symbols')
+      return
+    }
+  }
 
   return (
     <div className="login">
@@ -15,20 +51,49 @@ export const Login: React.FC = () => {
           <div className="login__inputs">
             <label>
               <p>Email:</p>
-              <input type="text" placeholder="Введите вашу почту..." />
+              <input
+                type="text"
+                name="email"
+                placeholder="Введите вашу почту..."
+                onChange={(event) => {
+                  setUserInfo({
+                    ...userInfo,
+                    email: event.target.value
+                  })
+                }}
+                value={userInfo.email}
+                required
+              />
             </label>
             <label>
               <p>Password:</p>
-              <input type="password" placeholder="Введите ваш пароль..." />
+              <input
+                type="password"
+                name="password"
+                placeholder="Введите ваш пароль..."
+                onChange={(event) => {
+                  setUserInfo({
+                    ...userInfo,
+                    password: event.target.value
+                  })
+                }}
+                value={userInfo.password}
+                required
+              />
             </label>
           </div>
           <div className="login__buttons">
             <label className="login__checkbox">
               <label htmlFor="">
-                {rememberMe && (<img src='/images/galochka.png' />)}
+                {userInfo.rememberMe && (<img src='/images/galochka.png' />)}
                 <input
                   type="checkbox"
-                  onClick={() => setRememberMe(!rememberMe)}
+                  onClick={() => 
+                    setUserInfo({
+                      ...userInfo,
+                      rememberMe: !userInfo.rememberMe
+                    })
+                  }
                 />
               </label>
               <p>Запомнить меня</p>
--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,8 +1,50 @@
 import React, { useState } from 'react';
 import './style/index.scss';
 
+type UserRegisterInfo = {
+  email: string,
+  password: string,
+  repeatPassword: string,
+  rememberMe: boolean,
+}
+
 export const Register: React.FC = () => {
-  const [ rememberMe, setRememberMe ] = useState<boolean>(false);
+  const [ userInfo, setUserInfo ] = useState<UserRegisterInfo>({
+    email: '',
+    password: '',
+    repeatPassword: '',
+    rememberMe: false,
+  })
+
+  const validation = () => {
+    const { email, password, repeatPassword, rememberMe } = userInfo;
+
+    if (email.length < 2 || email.length > 50) {
+      // i'll add modal block with error
+      alert('Your email length is not valid');
+      return
+    }
+
+    if (password.length < 8 || password.length > 20) {
+      alert('Your password length is not valid');
+      return
+    }
+
+    if (password.match(/[A-Z]/g)) {
+      alert('Your password must includes one symbol in uppercase')
+      return
+    }
+
+    if (password.match(/&?!-_$#*()/g)) {
+      alert('your password must includes this symbols')
+      return
+    }
+
+    if (repeatPassword === password)  {
+      alert('Your passwords are not same')
+      return;
+    }
+  }
 
   return (
   <div className="register">
@@ -15,24 +57,65 @@ export const Register: React.FC = () => {
           <div className="register__inputs">
             <label>
               <p>Email:</p>
-              <input type="text" placeholder="Введите вашу почту..." />
+              <input
+                type="text"
+                name="email"
+                placeholder="Введите вашу почту..."
+                onChange={(event) => {
+                  setUserInfo({
+                    ...userInfo,
+                    email: event.target.value
+                  })
+                }}
+                value={userInfo.email}
+                required
+              />
             </label>
             <label>
               <p>Password:</p>
-              <input type="password" placeholder="Придумайте пароль..."/>
+              <input
+                type="password"
+                name="password"
+                placeholder="Введите ваш пароль..."
+                onChange={(event) => {
+                  setUserInfo({
+                    ...userInfo,
+                    password: event.target.value
+                  })
+                }}
+                required
+                value={userInfo.password}
+              />
             </label>
             <label>
               <p>Confirm Password</p>
-              <input type="password" placeholder="Повторите пароль..." />
+              <input
+                type="password"
+                name="password"
+                placeholder="Введите ваш пароль..."
+                onChange={(event) => {
+                  setUserInfo({
+                    ...userInfo,
+                    password: event.target.value
+                  })
+                }}
+                required
+                value={userInfo.repeatPassword}
+              />
             </label>
           </div>
           <div className="register__buttons">
             <label className="register__checkbox">
-              <label htmlFor="">
-                {rememberMe && (<img src='/images/galochka.png' />)}
+            <label htmlFor="">
+                {userInfo.rememberMe && (<img src='/images/galochka.png' />)}
                 <input
                   type="checkbox"
-                  onClick={() => setRememberMe(!rememberMe)}
+                  onClick={() => 
+                    setUserInfo({
+                      ...userInfo,
+                      rememberMe: !userInfo.rememberMe
+                    })
+                  }
                 />
               </label>
               <p>Запомнить меня</p>
